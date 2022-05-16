528013cdbcffa46f98f788227aab4bb5f26219b6
morgane le gall[D <morganeleg@gmail.com>
2022-05-16 14:49:07 +0200

    2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work right now!"
+      @response = "great"
+    elsif @question.end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    else
+      @response = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
